cba472baa6c5411ebdab93d75f400d9d802f55c2
schmaeke <janschmaeke@gmail.com>
2023-12-22 19:29:47 +0100

    initial commit

---
+++ lua/night-grass/colors.lua
@@ -0,0 +1,50 @@
+local M = {}
+
+M.default = {
+	none = "NONE",
+	-- foreground
+	fg0 = "#79D2A6",
+	fg1 = "#9FDFBF",
+	fg2 = "#C6ECD9",
+	fg3 = "#D9F2E6",
+	-- background
+	bg0 = "#090E11",
+	bg1 = "#111C22",
+	bg2 = "#1A2A33",
+	bg3 = "#223844",
+	-- reds
+	r0 = "#A33D29",
+	r1 = "#DD573C",
+	r2 = "#D15E47",
+	r3 = "#DB8270",
+	-- greens
+	g0 = "#479438",
+	g1 = "#60CB4D",
+	g2 = "#8BCE7E",
+	g3 = "#ACDCA3",
+	-- yellows
+	y0 = "#A87C24",
+	y1 = "#E2A936",
+	y2 = "#E0B96C",
+	y3 = "#E9CD96",
+	-- blues
+	b0 = "#386694",
+	b1 = "#428CD7",
+	b2 = "#7EA6CE",
+	b3 = "#A3BFDC",
+	-- magentas
+	m0 = "#A63F73",
+	m1 = "#D7428C",
+	m2 = "#CE7EA6",
+	m3 = "#DCA3BF",
+	-- cyans
+	c0 = "#3FA695",
+	c1 = "#42D7BE",
+	c2 = "#7ECEC1",
+	c3 = "#A3DCD2",
+}
+
+function M.setup(opts)
+	palette = palette()
+
+	local colors = vim.tbl_deep_extend("force", vim.deepcopy(M.default), palette)
---
+++ lua/night-grass/init.lua
@@ -0,0 +1,1 @@
+local theme = require("neight-grass.theme")
